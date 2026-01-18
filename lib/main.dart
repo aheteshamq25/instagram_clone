406f0fe41ac29f5e3f57adf799cf5685236b1201
ahetesham <aheteq45@gmail.com>
2026-01-18 20:11:40 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive_layout/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive_layout/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive_layout/web_screen_layout.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: 'AIzaSyBcURWd-1qJTCyuH1vr4Xwj3mm6gswCR5Y',
+        appId: '1:502747988835:web:9c64f937f459483209c956',
+        messagingSenderId: "502747988835",
+        projectId: "instagram-clone-9ffde",
+        storageBucket: "instagram-clone-9ffde.firebasestorage.app",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
